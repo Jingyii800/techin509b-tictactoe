64e3b0f0d28d0898e377c5f946e90ddb8c97ec2e
Jingyii800 <evansjingyi@gmail.com>
2023-11-08 13:22:53 -0800

    update new week

--- cli.py
+++ cli.py
@@ -27,22 +27,5 @@ if __name__ == '__main__':
              print(board)
              print(winner, "won")
              break
-
-        # Update who's turn it is.
-        player = other_player(player)
-        # Show the board to the user.
-        print("Take a turn!")
-        print(board)
-
-        # input the move and upda
-        input_update_board(player, board)
-        # check if there is winner
-        winner = get_winner(board)
-        if winner:
-             # print the final board
-             print(winner, "won")
-             print(board)
-
-        # Update who's turn it is.
         player = other_player(player)
     
